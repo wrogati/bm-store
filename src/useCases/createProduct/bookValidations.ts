a07c4f414ca9dfb7cd70d621556a641f1b95a88f
Wellington Rogati <wellingtonrogati@gmail.com>
2022-10-02 15:18:29 -0300

    Created validations

--- src/useCases/createProduct/bookValidations.ts
+++ src/useCases/createProduct/bookValidations.ts
@@ -3,6 +3,7 @@ import * as Joi from "joi";
 export default Joi.object({
     title: Joi.string().min(3).max(30).required(),
     type: Joi.string().min(3).max(30).required(),
-    typeOfSale: Joi.string().email().required(),
-    salePrice: Joi.number().required(),
+    salePrice: Joi.number().min(1).required(),
+    rentPrice: Joi.number(),
+    typeOfSale: Joi.string()
 })
--- src/useCases/createProduct/movieValidations.ts
+++ src/useCases/createProduct/movieValidations.ts
@@ -3,7 +3,7 @@ import * as Joi from "joi";
 export default Joi.object({
     title: Joi.string().min(3).max(30).required(),
     type: Joi.string().min(3).max(30).required(),
-    typeOfSale: Joi.string().email().required(),
-    salePrice: Joi.string().email().required(),
-    rentPrice: Joi.string().email().required(),
+    typeOfSale: Joi.string().required(),
+    salePrice: Joi.number(),
+    rentPrice: Joi.number(),
 })
